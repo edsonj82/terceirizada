ae87453a5753a91b21f101b2ac7744aae089853c
Edson Jose dos Santos <edsonj82@gmail.com>
2019-10-28 09:10:00 -0300

    Login / Cadastro de Usuários - Validação de alguns campos obrigatorios

--- cypress/integration/escola/login.spec.js
+++ cypress/integration/escola/login.spec.js
@@ -6,7 +6,7 @@ describe("Login - home page no primeiro acesso", () =>{
         cy.visit('https://hom-terceirizadas.sme.prefeitura.sp.gov.br/')
     })
 
-    it('validar dados e imagem visiveis na tela', () => {
+    it('validar dados e imagem visiveis na tela de login', () => {
 
         cy.get('.login-bg').should('be.visible')
         
@@ -40,22 +40,153 @@ describe("Login - home page no primeiro acesso", () =>{
     })
 
     it('acessar SIGPAE', () => {
-        cy.get('[data-cy=email]').type('escola@admin.com');
-        cy.get('[data-cy=password]').type('adminadmin');
-        cy.get('[data-cy=Acessar]').click();        
-    });
+        cy.get('[data-cy=email]').type('escola@admin.com')
+        cy.get('[data-cy=password]').type('adminadmin')
+        cy.get('[data-cy=Acessar]').click()
+    })
     
-    // it('esqueci minha senha', () => {
-    //     cy.get('[data-cy=esqueci-minha-senha]').click();
-    // });
+    it('esqueci minha senha', () => {
+        //.mt-2 > .hyperlink
+        //TODO: FIX IT
+        cy.get('[data-cy=esqueci-minha-senha]').click()        
+    })
 
-    // it('Ainda não sou cadastrado', () => {
-    //     let faker = require('faker');
+    it('ainda não sou cadaastrado - validar dados e imagem visiveis na tela de cadastro do usuário', () => {
+
+        //TODO: FIX IT
+        // cy.get('a').click()
+
+        cy.get('.login-bg').should('be.visible')
+
+        cy.get('.logo-sigpae').should('be.visible')
+
+        cy.get('.col-form-label').should('contain','E-mail')
+        cy.get('[data-cy=email]').should('be.visible')
+
+        cy.get('.col-form-label').should('contain','Nome')
+        cy.get('[data-cy=nome').should('be.visible')
+
+        cy.get('.col-form-label').should('contain','Sobrenome')
+        cy.get('[data-cy=sobrenome').should('be.visible')
         
-    //     cy.get('[ainda-nao-sou-cadastrado]').click();
+        cy.get('.col-form-label').should('contain','RF')
+        cy.get('[data-cy=rf').should('be.visible')
 
-    //     let firstName  = faker.name.firstName();
-    //     let lastName = faker.name.lastName();
-    //     let rf = faker.internet.email().toLowerCase();
-    // });
+        cy.get('.col-form-label').should('contain','Vínculo')
+        cy.get('[data-cy=vinculo').should('be.visible')
+
+        cy.get('.col-form-label').should('contain','Senha')
+        cy.get('[data-cy=senha]').should('be.visible')
+        
+        cy.get('.col-form-label').should('contain','Senha')
+        cy.get('[data-cy=confirmar_senha]').should('be.visible')
+
+        //TODO: FIX IT
+        //cy.get('.#').should('be.visible')
+
+
+        //TODO: FIX IT
+        //cy.get('.#').should('be.visible')
+
+        cy.get('[data-cy=Cadastrar]').should('be.disabled')
+
+        cy.get('.logo-prefeitura').should('be.visible')
+    });
+
+    //TODO: FIX IT - Validar "Campos Obrigatórios"
+    it('validar cadastro desabilitado para campos nome sobrenome rf vinculo senha e confirmacao senha em branco', () => {
+        
+        cy.get('.col-form-label').should('contain','E-mail')
+
+        cy.get('[data-cy=Cadastrar]').should('be.disabled')
+    });
+
+    //TODO: FIX IT - Validar "Campos Obrigatórios"
+    it('validar cadastro desabilitado para campos nome sobrenome rf vinculo senha e confirmacao senha em branco', () => {
+        let faker = require('faker')
+
+        let email = faker.internet.email().toLowerCase()
+        cy.get('[data-cy=email]').type(email)
+
+        cy.get('[data-cy=Cadastrar]').should('be.disabled')
+    });
+
+    //TODO: FIX IT - Validar "Campos Obrigatórios"
+    it('validar cadastro desabilitado para campos sobrenome rf vinculo senha e confirmacao senha em branco', () => {
+        let faker = require('faker')
+
+        let email = faker.internet.email().toLowerCase()
+        cy.get('[data-cy=email]').type(email)
+
+        let nome = faker.internet.email().toLowerCase()
+        cy.get('[data-cy=email]').type(nome)
+
+        cy.get('[data-cy=Cadastrar]').should('be.disabled')
+    });
+
+    //TODO: FIX IT - Validar "Campos Obrigatórios"
+    it('validar cadastro desabilitado para campos rf vinculo senha e confirmacao senha em branco', () => {
+        let faker = require('faker')
+
+        let email = faker.internet.email().toLowerCase()
+        cy.get('[data-cy=email]').type(email)
+
+        let nome  = faker.name.firstName();
+        cy.get('[data-cy=email]').type(nome)
+
+        let sobrenome = faker.name.lastName();
+        cy.get('[data-cy=email]').type(sobrenome)
+
+        cy.get('[data-cy=Cadastrar]').should('be.disabled')
+    });
+
+    //TODO: FIX IT - Validar "Campos Obrigatórios"
+    it('validar cadastro desabilitado para campos vinculo senha e confirmacao senha em branco', () => {
+        let faker = require('faker')
+
+        let email = faker.internet.email().toLowerCase()
+        cy.get('[data-cy=email]').type(email)
+
+        let nome  = faker.name.firstName();
+        cy.get('[data-cy=email]').type(nome)
+
+        let sobrenome = faker.name.lastName();
+        cy.get('[data-cy=email]').type(sobrenome)
+
+        cy.get('[data-cy=rf]').type('987654321')
+
+        cy.get('[data-cy=Cadastrar]').should('be.disabled')
+    });
+
+    //TODO: FIX IT - Validar "Campos Obrigatórios"
+    it('validar cadastro desabilitado para campos vinculo confirmacao senha em branco', () => {
+        let faker = require('faker')
+
+        let email = faker.internet.email().toLowerCase()
+        cy.get('[data-cy=email]').type(email)
+
+        let nome  = faker.name.firstName();
+        cy.get('[data-cy=email]').type(nome)
+
+        let sobrenome = faker.name.lastName();
+        cy.get('[data-cy=email]').type(sobrenome)
+
+        cy.get('[data-cy=rf]').type('987654321')
+
+        cy.get('[data-cy=Cadastrar]').should('be.disabled')
+    });
+
+    it('validar confirmação de senha', () => {
+        
+    });
+
+    it('Ainda não sou cadastrado', () => {
+        let faker = require('faker');
+        
+        cy.get('[ainda-nao-sou-cadastrado]').click()
+
+        let firstName  = faker.name.firstName()
+        let lastName = faker.name.lastName()
+        let rf = faker.internet.email().toLowerCase()
+    })
 })
